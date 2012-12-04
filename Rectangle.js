c934c231089c9c04b8d4be2223e66f07227d05f6
Roshan Patel <roshanpatel27@gmail.com>
2012-12-03 22:52:24 -0500

    removed useless whitespace

--- Rectangle.js
+++ Rectangle.js
@@ -7,8 +7,8 @@ var Rectangle = Class.extend({
 	},
 	intersects : function(r2) {
 		return (this.y+this.h > r2.y) && 
-					(this.y < r2.y+r2.h) &&
-					(this.x+this.w >r2.x) &&
-					(this.x < r2.x+r2.w);
+			(this.y < r2.y+r2.h) &&
+			(this.x+this.w >r2.x) &&
+			(this.x < r2.x+r2.w);
 	}	
 });
